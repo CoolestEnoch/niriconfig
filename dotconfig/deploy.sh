1daba12b3a1b60d10cea99c481c2596c467ad5c8
CoolestEnoch <i@dont.know>
2025-10-14 11:19:21 +0800

    Auto patch for Surface Go 3 in deploy script; transparent workspace in front of wallpaper; remove wlr/taksbar

--- dotconfig/deploy.sh
+++ dotconfig/deploy.sh
@@ -10,10 +10,23 @@ fi
 
 cp -ruv * $HOME/.config
 
+PRODUCT_NAME=$(cat /sys/class/dmi/id/product_name 2>/dev/null)
+if [[ "$PRODUCT_NAME" == *"Surface"* ]]; then
+    echo "This is Surface: $PRODUCT_NAME"
+    CONFIG_FILE="$HOME/.config/niri/config.kdl"
+    
+    echo "Patching ~/.config/niri/config.kdl..."
+    sed -i 's/mode "1920x1080@60"/mode "1920x1280@60"/' "$CONFIG_FILE"
+    sed -i 's/scale 1.125/scale 1.25/' "$CONFIG_FILE"
+    sed -i 's/position x=1080 y=0/position x=1280 y=0/' "$CONFIG_FILE"
+    echo "Patching done."
+fi
+
 
 systemctl --user daemon-reload
 systemctl --user add-wants niri.service swaybg.service
 systemctl --user add-wants niri.service swaync_auto.service
+systemctl --user add-wants niri.service --now vicinae.service
 
 
 pkill waybar
